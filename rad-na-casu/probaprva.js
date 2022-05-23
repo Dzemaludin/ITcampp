c7a8ad9ff6af827292670f70225efa3218cf448a
Dzemaludin Oraovcanin <utko1998.do@gmail.com>
2022-05-23 22:50:04 +0200

    js

---
+++ rad-na-casu/probaprva.js
@@ -0,0 +1,318 @@
+
+// a = "dzebra"
+// dan = 10
+
+
+// var1 = prompt("Unesite vrednost")
+// console.log(2022 - var1)
+
+
+// godina = 1998;
+// mesec = 6;
+// dan = 3;
+
+// console.log("Imam",)
+// console.log()
+
+
+
+
+// broj = 10 
+
+// if (true) {
+//     console.log("u if bloku");
+
+// }
+// if (dan <= 30) {
+//     console.log()
+
+  
+// // 
+// console.log(dan);
+// console.log(a);
+
+// c = Boolean(a)
+// console.log(c);
+// d = prompt("kakaocemo")
+// console.log();
+
+// for (i =1 ; i <= 10; i++) {
+//     console.log("hello world" ,i);
+// }
+// for (i =10 ; i >= 1; i--) {
+//     console.log("hello world" ,i);
+// }
+// for (i =1 ; i <= 10; i++) {
+//     console.log("Unosimo, policu, broj" ,i);
+// }
+
+// for (i =1 ; i <= 10; i++) {
+//     if(i !== 7){
+//     console.log(i )
+   
+//     }
+
+// }
+
+// for (i =1 ; i <= 10; i++){
+//     if (i ===7){
+//         continue;
+//     }
+//     console.log(i);
+// }
+// for (i =1 ; i <= 10; i++){
+//     if (i ===7){
+//         break;
+//     }
+//     console.log(i);
+
+// }
+// suma = 0
+// for (i = 1 ; i <= 17 ; i++){
+//     console.log(i);
+//     suma = suma + i
+// }
+// console.log(suma);
+
+
+
+// for(i = 0; i < 6; i++){
+//     if(i < 3){
+//         console.log("Python");
+//     }
+//     else{
+//         console.log("je super")
+//     }
+// }
+
+
+// for(i = 0; i < 6; i++){
+//     if(i < 3){
+//         console.log("Python");
+//         continue;
+//     }
+//         console.log("je super")
+// }
+
+
+// b = prompt("Unesi broj")
+
+// for(i = b; i != 0; i--){
+//     console.log(i);
+// }
+
+
+
+// b = prompt("Unesi broj")
+
+// for(i = 2; i <= b; i += 2){
+//     console.log(i)
+// }
+
+// bbrojeva = Number(prompt("unesite broj brojeva"));
+// brojparnih = 0;
+// brojneparnih = 0 ;
+
+// sumaparnih = 0 ;
+// sumaneparnih = 0 ;
+
+// for(i = 1; i < bbrojeva; i ++ ){
+//     broj = Number (prompt ("unesite broj brojeva"));
+
+//     if(broj % 2 === 0){
+//         console.log("Broj je paran");
+//         brojparnih++;
+//     } else {
+//         console.log("Broj je neparan");
+//         brojneparnih++;
+//     }
+// }
+
+// console.log("Br parnih", brojparnih);
+// console.log("Br parnih", brojneparnih);
+
+// unesibr = Number(prompt("unesite broj"));
+
+
+// for(i = 0 ; i <=unesibr ; i++ ){
+//     // bbrojeva = Number(prompt("unesite broj"));
+//     // console.log();
+//     if ( broj % i === 0 ) {
+//         console.log(i);
+//     }
+// }
+
+// // zvezdica
+
+// broj = Number(prompt("unesite broj zvezdica "));
+// zvezdica = "" ;
+// for(i=0 ; i <=broj ; i++) {
+//     zvezdica = zvezdica + "*"
+//     console.log(zvezdica);
+        
+//     }
+
+// a = 0
+// while(a < 5) {
+//     console.log("Daris");-
+// }
+
+// // log inf
+
+// sifra = "dzema123";
+// unos = prompt("Unesite sifru");
+// while( unos !== sifra){
+//     unos = prompt("Unesite sifru")
+// }
+// console.log("Uspesno ste ulogovani");
+
+
+// // nije zavrsen brojac imas sliku
+// kollicina = parseInt(prompt("unesite neki broj"));
+// brojac = 0;
+// broj = 0;
+
+// while (brojac != kollicina){
+//     if (broj %2 === 1) {
+
+//     }
+// }
+
+// //fezz buzz
+// brojac = 1;
+
+// while(brojac<100){
+
+    
+//     if (brojac % 3 === 0 && brojac %5 === 0){
+//         console.log("fezzbuzz");
+//     }
+//     else if(brojac %3 === 0){
+//         console.log("fezz");
+//     }
+//     else if(brojac %5 === 0){
+//         console.log("buzz");
+//     }
+//     else {
+//         console.log(brojac);
+//     }
+//    brojac++
+// }
+
+
+
+// unos = parseInt(prompt("unesite neki broj "));
+// brojac = 0;
+// zvezdica = "" ;
+// while(brojac < unos) {
+//     zvezdica = zvezdica + "*"
+//     console.log(zvezdica);
+//     brojac++
+        
+//     }
+
+
+
+
+
+
+
+// //drugi zadatak ja radio
+
+// ime = "Dzemaludin";
+// unos = prompt("Unesite ime");
+// while( unos !== ime){
+//     unos = prompt("Unesite ime")
+// }
+// console.log("I am done");
+
+
+
+
+
+// pass = ""
+// while (pass !== "sifra123");{
+    //     console.log("aaaa");
+    //     pass = prompt("unesite sifru");
+    // }
+
+    
+    
+    // u ptomtu trazimo brojeve i uperacije kad napisemo exit da izadje iz programa
+    
+    
+    
+    // operaciajaa = ""
+    // brojevi = ""
+    
+    // while() {
+        //     broj = prompt("Unesite broj");
+        //     operacija = prompt("Unesi operaciju");
+//     if operacijaa === +
+//     console.log(broj + );
+
+// }    
+
+
+
+// a = [1,1,2,3,3,4,5,5,6,7]
+
+// console.log([1,1,2,3,3,4,5,5,6,7])
+
+
+
+
+//  nizz
+
+// niz = [3,5,21,83,12,93,12,41,28,63,1]
+// suma = 0
+
+// for(i = 0 ; i < niz.length ; i++ ){
+//     suma += niz[i]
+    
+// }
+// console.log(suma);
+// console.log(suma/ niz.length);
+
+
+
+
+// broj_unosa = prompt("Unesi broj brojeva")
+// suma = 0
+// niz = []
+
+// for(i = 0 ; i < broj_unosa ; i++ ){
+//    niz[i] = prompt("unesi elemente")
+
+// }
+// console.log(niz);
+
+// niz = [1,2,3,4,5,6,7,8]
+
+// prazan-niz =[]
+ 
+//  for(i = 0 ; i < niz.length ; i++ ){
+//     prazan-niz.(push)
+
+//  }
+
+// niz = [1,2,3,4,5]
+// broj = promt("Unesi neki broj")
+// postojanje = false
+// for(i = 0 ; i < niz.length ; i++){
+     
+// }
+
+
+
+// niz = [1,2,3,4,5]
+
+// unos = Number(prompt("Unesi neki broj"))
+// console.log(niz [i]);
+
+// for(i = 0 ; i < niz.length ; i++ ){
+    
+// }
+
+
+
